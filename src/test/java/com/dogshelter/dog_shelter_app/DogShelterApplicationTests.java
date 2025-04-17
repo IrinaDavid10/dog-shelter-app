f2b4c22ff41c04aa2e79d581fcd26f7c1aa26727
Irina <irina.david@cimadata.ro>
2025-04-17 12:49:14 +0300

    Initial project

---
+++ src/test/java/com/dogshelter/dog_shelter_app/DogShelterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dogshelter.dog_shelter_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogShelterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
